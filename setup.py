2a3f40c15d2fb8ba1004d3b20b4cac6d64b9335e
Heckler <77711811+Programming-geek582@users.noreply.github.com>
2022-04-27 20:48:16 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'PG Rest api wrapper'
+LONG_DESCRIPTION = 'The official rest api wrapper for the pg api made by programming geek'
+
+# Setting up
+setup(
+    name="pg-wrapper",
+    version=VERSION,
+    author="Programming geek",
+    author_email="htmlgeek752@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['aiohttp', 'requests'],
+    keywords=['python', 'api', 'api_wrapper'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
